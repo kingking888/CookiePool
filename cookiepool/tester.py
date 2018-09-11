f79b7da633818b57138bbcaead33788d586f5237
zhouyi <zhouyi@dajiaok.com>
2018-09-11 20:11:01 +0800

    cookie init

---
+++ cookiepool/tester.py
@@ -0,0 +1,20 @@
+import json
+import requests
+from requests.exceptions import ConnectionError
+from cookiepool.db import *
+
+
+class ValidTester(object):
+    def __init__(self, website='default'):
+        self.website = website
+        self.cookies_db = RedisClient('cookies', self.website)
+        self.accounts_db = RedisClient('accounts', self.website)
+    
+    def test(self, username, cookies):
+        raise NotImplementedError
+    
+    def run(self):
+        cookies_groups = self.cookies_db.all()
+        for username, cookies in cookies_groups.items():
+            self.test(username, cookies)
+
